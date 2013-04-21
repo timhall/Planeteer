4ff53bfdc296b5fbc13b2c0c381b75968fc6cdb6
Tim Hall <tim.hall.engr@gmail.com>
2013-04-21 16:31:43 -0400

    Add build files

---
+++ js/cosmic/build/cosmic.js
@@ -0,0 +1,16 @@
+var cosmic = (function(global, _, Backbone, undefined){
+    "use strict";
+
+    // Define and export the freebody namespace
+    var cosmic = {};
+
+// @include ../environment.js
+// @include ../CameraBase.js
+// @include ../collisions.js
+// @include ../Matter.js
+// @include ../gamePiece.js
+// @include ../KineticCamera.js
+// @include ../main.js
+
+    return cosmic;
+})(this, _, Backbone);
